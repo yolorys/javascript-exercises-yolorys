d13c67cce9a4cd98bd9455fd8e382537bd11eb79
yolorys <yelarysseidin05@gmail.com>
2025-05-29 02:36:01 -0500

    Completed the 7th. Have done similar problem using Python before.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(arr) {
+    let res = "";
+    for (let i = arr.length - 1; i >= 0; i--){
+        res += arr[i];
+    }
 
+    return res;
 };
 
 // Do not edit below this line
