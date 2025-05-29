43afd732d0b4bcdf465120e75a60e8fc772387ce
yolorys <yelarysseidin05@gmail.com>
2025-05-29 02:25:19 -0500

    Completed the 6th exercise. Nice.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n < 0) return "ERROR";
 
+    let res = "";
+    for (let i = 0; i < n; i++){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
