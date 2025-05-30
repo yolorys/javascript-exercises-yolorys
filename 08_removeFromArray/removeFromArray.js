322f74c2f954dc30fb814d536c4aa390a1f14384
yolorys <yelarysseidin05@gmail.com>
2025-05-30 00:32:28 -0500

    Completed the 8th. Need to explore more js methods

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    return arr.filter(item => !itemsToRemove.includes(item));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
