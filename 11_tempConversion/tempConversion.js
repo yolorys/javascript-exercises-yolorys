12a2bd68f67cd57981b0b8773653389974409f4e
yolorys <yelarysseidin05@gmail.com>
2025-05-30 01:22:37 -0500

    Completed the 11th. Not too bad.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Math.round((5/9 * (f - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Math.round((9/5 * c + 32) * 10) / 10;
 };
 
 // Do not edit below this line
