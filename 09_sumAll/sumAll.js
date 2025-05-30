2a80c7dcf6f0cefcd61146d2af107cf6f209bf19
yolorys <yelarysseidin05@gmail.com>
2025-05-30 00:53:46 -0500

    Completed the 9th. Not bad

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }
+    }
+    else{
+        for (let i = b; i <= a; i++){
+            sum += i;
+        }   
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
