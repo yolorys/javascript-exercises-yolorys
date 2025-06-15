b8d748d8784dbf3f0c209032c824f92140395684
yolorys <yelarysseidin05@gmail.com>
2025-06-15 12:05:53 -0500

    Completed the last, 16th. Nice.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const currentYear = new Date().getFullYear();
+    people.sort((a, b) => {
+        return ((a.yearOfDeath || currentYear) - a.yearOfBirth) -
+         ((b.yearOfDeath || currentYear) - b.yearOfBirth)
+    });
+
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
