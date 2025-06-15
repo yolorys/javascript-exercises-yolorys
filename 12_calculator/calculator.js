c15d4ab52af21cce2e77bc8bf978e3911e8fba92
yolorys <yelarysseidin05@gmail.com>
2025-06-15 10:20:00 -0500

    Finished the 12th. Good.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a ,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, p) {
+	return Math.pow(a, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) {
+    return -1; // Factorial is not defined for negative numbers
+  }
+  if (n === 0 || n === 1) {
+    return 1; // Base case: 0! and 1! are 1
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
