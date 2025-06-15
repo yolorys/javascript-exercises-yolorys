b8956ac28eadee8eb69d29d713012a5e800ca3de
yolorys <yelarysseidin05@gmail.com>
2025-06-15 11:13:35 -0500

    Completed the 14th. Well done.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n);
 
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+
+  const fib = [1, 1];
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[n-1];
 };
 
 // Do not edit below this line
