3ced3b4fa32c3b0f8ed530363fbf5658de5725ba
yolorys <yelarysseidin05@gmail.com>
2025-06-15 10:59:21 -0500

    Completed the 13th. Fun one.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-zA-Z0-9\s]/g, "");
+    str = str.replaceAll(" ", "");
 
+    let backward = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        backward += str[i];
+    }
+
+    return str == backward;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
